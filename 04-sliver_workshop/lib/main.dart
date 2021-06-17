2da2da59d431b8c6ebead31abb1e8d84d694b804
Earl Panganiban <earl@quadrivium.xyz>
2021-06-17 22:20:46 +0800

    Replaced main.dart with starter code

--- 04-sliver_workshop/lib/main.dart
+++ 04-sliver_workshop/lib/main.dart
@@ -1,113 +1,187 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(HorizonsApp());
 }
 
-class MyApp extends StatelessWidget {
+class HorizonsApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
+        debugShowCheckedModeBanner: false,
         // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+        theme: ThemeData.dark(),
+        // Scrolling in Flutter behaves differently depending on the
+        // ScrollBehavior. By default, ScrollBehavior changes depending
+        // on the current platform. For the purposes of this scrolling
+        // workshop, we're using a custom ScrollBehavior so that the
+        // experience is the same for everyone - regardless of the
+        // platform they are using.
+        scrollBehavior: const ConstantScrollBehavior(),
+        title: 'Horizons Weather',
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('Horizons'),
+            backgroundColor: Colors.teal[800],
+          ),
+          body: WeeklyForecastList(),
+        ));
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class WeeklyForecastList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+    final DateTime currentDate = DateTime.now();
+    final TextTheme textTheme = Theme.of(context).textTheme;
+    final List<DailyForecast> forecasts = Server.getDailyForecastList();
+
+    return SingleChildScrollView(
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      children: forecasts.map((DailyForecast dailyForecast) {
+        return Card(
+          child: ListTile(
+            leading: Text(
+              dailyForecast.getDate(currentDate.day).toString(),
+              style: textTheme.headline4,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            title: Text(
+              dailyForecast.getWeekday(currentDate.weekday),
+              style: textTheme.headline5,
             ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+            subtitle: Text(dailyForecast.description),
+            trailing: Text(
+              '${dailyForecast.highTemp} | ${dailyForecast.lowTemp} F',
+              style: textTheme.subtitle2,
+            ),
+          ),
+        );
+      }).toList(),
+    ));
   }
+}
+
+// --------------------------------------------
+// Below this line are helper classes and data.
+
+const String baseAssetURL =
+    'https://dartpad-workshops-io2021.web.app/getting_started_with_slivers/';
+const String headerImage = '${baseAssetURL}assets/header.jpeg';
+
+const Map<int, DailyForecast> _kDummyData = {
+  0: DailyForecast(
+    id: 0,
+    imageId: '${baseAssetURL}assets/day_0.jpeg',
+    highTemp: 73,
+    lowTemp: 52,
+    description:
+        'Partly cloudy in the morning, with sun appearing in the afternoon.',
+  ),
+  1: DailyForecast(
+    id: 1,
+    imageId: '${baseAssetURL}assets/day_1.jpeg',
+    highTemp: 70,
+    lowTemp: 50,
+    description: 'Partly sunny.',
+  ),
+  2: DailyForecast(
+    id: 2,
+    imageId: '${baseAssetURL}assets/day_2.jpeg',
+    highTemp: 71,
+    lowTemp: 55,
+    description: 'Party cloudy.',
+  ),
+  3: DailyForecast(
+    id: 3,
+    imageId: '${baseAssetURL}assets/day_3.jpeg',
+    highTemp: 74,
+    lowTemp: 60,
+    description: 'Thunderstorms in the evening.',
+  ),
+  4: DailyForecast(
+    id: 4,
+    imageId: '${baseAssetURL}assets/day_4.jpeg',
+    highTemp: 67,
+    lowTemp: 60,
+    description: 'Severe thunderstorm warning.',
+  ),
+  5: DailyForecast(
+    id: 5,
+    imageId: '${baseAssetURL}assets/day_5.jpeg',
+    highTemp: 73,
+    lowTemp: 57,
+    description: 'Cloudy with showers in the morning.',
+  ),
+  6: DailyForecast(
+    id: 6,
+    imageId: '${baseAssetURL}assets/day_6.jpeg',
+    highTemp: 75,
+    lowTemp: 58,
+    description: 'Sun throughout the day.',
+  ),
+};
+
+class Server {
+  static List<DailyForecast> getDailyForecastList() =>
+      _kDummyData.values.toList();
+
+  static DailyForecast getDailyForecastByID(int id) {
+    assert(id >= 0 && id <= 6);
+    return _kDummyData[id]!;
+  }
+}
+
+class DailyForecast {
+  const DailyForecast({
+    required this.id,
+    required this.imageId,
+    required this.highTemp,
+    required this.lowTemp,
+    required this.description,
+  });
+  final int id;
+  final String imageId;
+  final int highTemp;
+  final int lowTemp;
+  final String description;
+
+  static const List<String> _weekdays = <String>[
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+    'Sunday',
+  ];
+
+  String getWeekday(int today) {
+    final int offset = today + id;
+    final int day = offset >= 7 ? offset - 7 : offset;
+    return _weekdays[day];
+  }
+
+  int getDate(int today) => today + id;
+}
+
+class ConstantScrollBehavior extends ScrollBehavior {
+  const ConstantScrollBehavior();
+
+  @override
+  Widget buildScrollbar(
+          BuildContext context, Widget child, ScrollableDetails details) =>
+      child;
+
+  @override
+  Widget buildOverscrollIndicator(
+          BuildContext context, Widget child, ScrollableDetails details) =>
+      child;
+
+  @override
+  TargetPlatform getPlatform(BuildContext context) => TargetPlatform.macOS;
+
+  @override
+  ScrollPhysics getScrollPhysics(BuildContext context) =>
+      const BouncingScrollPhysics(parent: AlwaysScrollableScrollPhysics());
 }
