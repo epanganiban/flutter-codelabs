fa0840029bcbcd1df976522b775367990f4ff68f
Earl Panganiban <earl@quadrivium.xyz>
2021-06-18 17:02:44 +0800

    Completed step 4

--- 05-null_safety/bin/04-null_assertion.dart
+++ 05-null_safety/bin/04-null_assertion.dart
@@ -1,12 +1,12 @@
 int? couldReturnNullButDoesnt() => -3;
+int? couldBeNullButIsnt = 1;
 
 void main() {
-  int? couldBeNullButIsnt = 1;
   List<int?> listThatCouldHoldNulls = [2, null, 4];
 
-  int a = couldBeNullButIsnt;
-  int b = listThatCouldHoldNulls.first; // first item in the list
-  int c = couldReturnNullButDoesnt().abs(); // absolute value
+  int a = couldBeNullButIsnt!;
+  int b = listThatCouldHoldNulls.first!; // first item in the list
+  int c = couldReturnNullButDoesnt()!.abs(); // absolute value
 
   print('a is $a.');
   print('b is $b.');
