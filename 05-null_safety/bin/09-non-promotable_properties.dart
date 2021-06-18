f5608ab5944506168e737abd11c844c48b1b4b48
Earl Panganiban <earl@quadrivium.xyz>
2021-06-18 17:28:41 +0800

    Added starter code for step 9

---
+++ 05-null_safety/bin/09-non-promotable_properties.dart
@@ -0,0 +1,18 @@
+import 'dart:math';
+
+class RandomStringProvider {
+  String? get value => Random().nextBool() ? 'A String' : null;
+}
+
+void printString(String str) => print(str);
+
+void main() {
+  final provider = RandomStringProvider();
+
+  if (provider.value == null) {
+    print('The value is null.');
+  } else {
+    print('The value is not null, so we print it');
+    printString(provider.value);
+  }
+}
