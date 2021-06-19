97612cfcc3ec8ae433dff3d3fdcfd8de31d00c7d
Earl Panganiban <earl@quadrivium.xyz>
2021-06-19 13:40:00 +0800

    Added TODO for step 5

--- 06-inherited_widgets/lib/main.dart
+++ 06-inherited_widgets/lib/main.dart
@@ -8,6 +8,7 @@ final GlobalKey<ProductListWidgetState> productList =
     GlobalKey<ProductListWidgetState>();
 
 void main() {
+  // TODO: insert AppStateWidget above MaterialApp.
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     title: 'Store',
