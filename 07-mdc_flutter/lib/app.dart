461a807fef8f96f0f326cd5883a27b3dbb7a57d8
Earl Panganiban <earl@quadrivium.xyz>
2021-06-22 09:39:30 +0800

    Applied theme typography

--- 07-mdc_flutter/lib/app.dart
+++ 07-mdc_flutter/lib/app.dart
@@ -59,8 +59,38 @@ ThemeData _buildShrineTheme() {
       secondary: kShrineBrown900,
       error: kShrineErrorRed,
     ),
+    textTheme: _buildShrineTextTheme(base.textTheme),
+    primaryTextTheme: _buildShrineTextTheme(base.primaryTextTheme),
+    textSelectionTheme: TextSelectionThemeData(
+      selectionColor: kShrinePink100,
+    ),
     appBarTheme: base.appBarTheme.copyWith(
       backwardsCompatibility: false,
     ),
   );
+}
+
+TextTheme _buildShrineTextTheme(TextTheme base) {
+  return base
+      .copyWith(
+        headline5: base.headline5!.copyWith(
+          fontWeight: FontWeight.w500,
+        ),
+        headline6: base.headline6!.copyWith(
+          fontSize: 18.0,
+        ),
+        caption: base.caption!.copyWith(
+          fontWeight: FontWeight.w400,
+          fontSize: 14.0,
+        ),
+        bodyText1: base.bodyText1!.copyWith(
+          fontWeight: FontWeight.w500,
+          fontSize: 16.0,
+        ),
+      )
+      .apply(
+        fontFamily: 'Rubik',
+        displayColor: kShrineBrown900,
+        bodyColor: kShrineBrown900,
+      );
 }
